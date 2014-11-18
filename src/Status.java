f3c2827ed5f97013e77b78a694c37eaf38087e20
oscar719 <oscar.ng@aggiemail.usu.edu>
2014-11-17 20:51:09 -0700

    Bulid status class.

--- src/Status.java
+++ src/Status.java
@@ -1,4 +1,57 @@
 
-public class Status {
+public class Status 
+{
+	int health;
+	int magika;
+	int stamina;
+	
+	public Status(int health, int magika, int stamina)
+	{
+		if(health < 1)
+			this.health = 1;
+		else
+			this.health = health;
+		
+		if(magika < 1)
+			this.magika = 1;
+		else
+			this.magika = magika;
+		
+		if(stamina < 1)
+			this.stamina = 1;
+		else
+			this.stamina = stamina;
+	}
+	
+	public void update(Status s)
+	{
+		health += s.getHealth();
+		magika += s.getMagika();
+		stamina += s.getStamina();
+	}
 
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getMagika() {
+		return magika;
+	}
+
+	public void setMagika(int magika) {
+		this.magika = magika;
+	}
+
+	public int getStamina() {
+		return stamina;
+	}
+
+	public void setStamina(int stamina) {
+		this.stamina = stamina;
+	}
+	
 }
