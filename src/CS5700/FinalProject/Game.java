536d102ee4f8fa1b5823335f3836226489ea5975
oscar719 <oscar.ng@aggiemail.usu.edu>
2014-11-18 15:43:23 -0700

    Created a game class for control the game.

--- src/CS5700/FinalProject/Game.java
+++ src/CS5700/FinalProject/Game.java
@@ -1,5 +1,10 @@
 package CS5700.FinalProject;
 
-public class Game {
-
+public class Game 
+{
+	private Character p1;
+	private Character p2;
+	private Character e1;
+	private Character e2;
+	private Character e3;
 }
